f9c070f509ebec0ef04680ef3002ed226033c358
Harrison Muskat <harrisonmuskat@gmail.com>
2017-05-18 15:53:40 -0400

    react added

--- spec/features/01_user_visits_home_page_spec.rb
+++ spec/features/01_user_visits_home_page_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
-feature "user visits main page" do
-  scenario "sees and clicks on Sign In button from main page" do
+feature "user visits main page and signs up" do
+  scenario "sees Sign In button from main page" do
     visit root_path
 
     expect(page).to have_link("Sign In")
     expect(page).to have_content("On the Rocks")
+  end
+
+  scenario "user clicks Sign In button and sees fields to sign in" do
+    visit root_path
+
+    expect(page).to have_content("Email address")
+    expect(page).to have_content("Password")
   end
 end
